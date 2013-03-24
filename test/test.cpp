c82d09077fd2bbe3b67e8ff97f001d654edf6851
Mike Naquin <naquin.code@gmail.com>
2013-03-24 08:30:11 -0500

    Convert unit tests to google test

--- test/test.cpp
+++ test/test.cpp
@@ -10,50 +10,8 @@ Description:
 	when running main().
 */
 
-#include <iostream>
-#include <stdexcept>
+#include <gtest/gtest.h>
 
-using std::cout;
-using std::cerr;
-using std::endl;
-
-void test_percent(std::ostream& out);
-void test_int    (std::ostream& out);
-void test_str    (std::ostream& out);
-void test_ref    (std::ostream& out);
-void test_const  (std::ostream& out);
-void test_date   (std::ostream& out);
-
-void example();
-
-int main(int const argc, char* argv[])
-{
-	try {
-		test_percent(cout);
-		test_int    (cout);
-		test_str    (cout);
-		test_ref    (cout);
-		test_const  (cout);
-		test_date   (cout);
-
-		cout << '\n' << "--== Success ==--" << endl;
-		return 0;
-	}
-	catch ( std::exception const& e ) {
-		cerr << e.what() << endl;
-	}
-	catch (...) {
-		cerr << "An unknown exception has occurred." << endl;
-	}
-
-	cerr << '\n' << "--== Failure ==--" << endl;
-	return 1;
-}
-
-
-#include <boost/assert.hpp>
-
-//#include "safe_data/safe_data.h"
 #include "safe_data/safe.h"
 #include "safe_data/values.h"
 #include "safe_data/validations.h"
@@ -76,14 +34,14 @@ using safe_data::str_length_exception;
 
 using boost::mpl::integral_c;
 using boost::mpl::int_;
-      
+
 using safe_data::min_validation;
 using safe_data::max_validation;
 using safe_data::range_validation;
 using safe_data::str_length_validation;
 
 SAFE_DATA_INITIAL_VALUE(percent_min, double, 0.0)
-SAFE_DATA_INITIAL_VALUE(percent_max, double, 0.0)
+SAFE_DATA_INITIAL_VALUE(percent_max, double, 1.0)
 
 template <class T, class min_value, class max_value>
 struct percent_exception : public range_exception<T, min_value, max_value> {
@@ -126,89 +84,60 @@ struct percent_validation : public range_validation<T, min_value, max_value, exc
 
 typedef safe<double, percent_validation<double> > percent;
 
-void test_percent(std::ostream& out)
+TEST(SafeDataTest, Percent)
 {
-	out << " - percent test - " << endl;
 	percent p;
 
-	BOOST_ASSERT(p == 0);
-	out << "initial value: " << p << endl;
+	EXPECT_EQ(0, p);
 
 	p = 0.5;
-	BOOST_ASSERT(p == 0.5);
-	out << "assign to 50%: " << p << endl;
+	EXPECT_EQ(0.5, p);
 
-	try {
-		// test against min value
-		p = -1.5;
-	}
-	catch (percent::validation_type::exception_type const& e) {
-		out << "expected exception caught: " << e.what() << endl;
-	}
+    // test against min value
+    EXPECT_THROW(p = -1.5,
+                 percent::validation_type::exception_type);
 
 	// should still be 50% at this point
-	BOOST_ASSERT(p == 0.5);
+	EXPECT_EQ(0.5, p);
 
-	try {
-		// test against max value
-		p = 2;
-	}
-	catch (percent::validation_type::exception_type const& e) {
-		out << "expected exception caught: " << e.what() << endl;
-	}
+    // test against max value
+    EXPECT_THROW(p = 2, percent::validation_type::exception_type);
+    
+	EXPECT_EQ(0.5, p);
 
-	BOOST_ASSERT(p == 0.5);
-
-	p = 1;
-	out << "assign to 100%: " << p << endl;
+	EXPECT_NO_THROW(p = 1);
 
 	// test swap
 	percent p2;
 	p.swap(p2);
 
-	BOOST_ASSERT(p  == 0.0);
-	BOOST_ASSERT(p2 == 1.0);
-
-	out << "swap p and p2 (" << p << ", " << p2 << ")" << endl;
-
-	out << endl;
+	EXPECT_EQ(0.0, p);
+	EXPECT_EQ(1.0, p2);
 }
 
 // int test
 typedef safe<int, max_validation<int, int_<32> >, int_<8> > safe_int;
 
-void test_int(std::ostream& out)
+TEST(SafeDataTest, Int)
 {
-	out << " - int max test - " << endl;
 	safe_int i; // initial value set to 8
 
-	BOOST_ASSERT(i == 8);
-	out << "initial value: " << i << endl;
+	EXPECT_EQ(8, i);
 
 	i = 16;
-	BOOST_ASSERT(i == 16);
-	out << "assign to 16: " << i << endl;
+	EXPECT_EQ(16, i);
 
 	++i;
-	BOOST_ASSERT(i == 17);
-	out << "increment to 17: " << i << endl;
+	EXPECT_EQ(17, i);
 
-	try {
-		// test against max value
-		i = 33;
-	}
-	catch (safe_int::validation_type::exception_type const& e) {
-		out << "expected exception caught: " << e.what() << endl;
-	}
+    // test against max value
+    EXPECT_THROW(i = 33, safe_int::validation_type::exception_type);
 
-	BOOST_ASSERT(i == 17);
+	EXPECT_EQ(17, i);
 
 	// no min validation
 	i = -128;
-	BOOST_ASSERT(i = -128);
-	out << "assign to -128: " << i << endl;
-
-	out << endl;
+	EXPECT_NO_THROW(i = -128);
 }
 
 #include "safe_data/compare.h"
@@ -222,75 +151,44 @@ typedef safe<string, str_length_validation<string, boost::mpl::size_t<8> >, str_
 
 void test_str(std::ostream& out)
 {
-	out << " - string length test - " << endl;
 	safe_str s; // initial "foo"
 
-	BOOST_ASSERT(s == "foo"); // use boost/safe_data/compare.h or get(s) or s.data()
-	out << "initial value: " << s << endl; // use boost/safe_data/io.h or get(s) or s.data()
+	EXPECT_EQ("foo", s); // use boost/safe_data/compare.h or get(s) or s.data()
 
 	s += " bar"; // use boost/safe_data/operators.h or s = get(s) + " bar"
-	BOOST_ASSERT(s == "foo bar");
-	out << "concatination: " << s << endl;
+	EXPECT_EQ("foo bar", s);
 
-	try {
-		s = "long " + get(s); // operator+ cannot const char* to string
-		                      // can also string("long ") + s
-		                      // or "long " + s.data()
-	}
-	catch (safe_str::validation_type::exception_type const& e ) {
-		out << "expected exception caught: " << e.what() << endl;
-	}
+	EXPECT_THROW(s = "long " + get(s), // operator+ cannot const char* to string
+                                       // can also string("long ") + s
+                                       // or "long " + s.data()
+                 safe_str::validation_type::exception_type);
 
-	BOOST_ASSERT(s == "foo bar");
-
-	out << endl;
+	EXPECT_EQ("foo bar", s);
 }
 
 typedef safe<double&, range_validation<double, int_<0>, int_<10> > > safe_dbl;
 
 void test_ref(std::ostream& out)
 {
-	out << " - double reference test - " << endl;
-
 	double pod_d = 3.14;
-	out << "POD initial: " << pod_d << endl;
 
 	safe_dbl safe_d(pod_d);
-	BOOST_ASSERT(safe_d == 3.14);
-	out << "safe initial: " << safe_d << endl;
+	EXPECT_EQ(3.14, safe_d);
 
-	try {
-		safe_d *= 4;
-	}
-	catch ( safe_dbl::validation_type::exception_type const& e )  {
-		out << "expected exception caught: " << e.what() << endl;
-	}
+	EXPECT_THROW(safe_d *= 4, safe_dbl::validation_type::exception_type);
 
-	BOOST_ASSERT(safe_d == 3.14);
-	BOOST_ASSERT(pod_d  == 3.14);
+	EXPECT_EQ(3.14, safe_d);
+	EXPECT_EQ(3.14, pod_d);
 
 	safe_d /= 2;
-	BOOST_ASSERT(safe_d == 1.57);
-	BOOST_ASSERT(pod_d  == 1.57);
-
-	out << "safe double division: " << safe_d << endl;
-	out << "reference value: "      << pod_d << endl;
+	EXPECT_EQ(1.57, safe_d);
+	EXPECT_EQ(1.57, pod_d);
 
 	pod_d = 100; // setting reference value will not be validated
-	BOOST_ASSERT(pod_d  == 100);
-	BOOST_ASSERT(safe_d == 100);
+	EXPECT_EQ(100, pod_d);
+	EXPECT_EQ(100, safe_d);
 
-	out << "safe double cannot check reference change: " << safe_d << endl;
-	out << "reference value: "      << pod_d << endl;
-
-	try {
-		safe_d.validate();
-	}
-	catch ( safe_dbl::validation_type::exception_type const& e ) {
-		out << "expected exception caught: " << e.what() << endl;
-	}
-
-	out << endl;
+	EXPECT_THROW(safe_d.validate(), safe_dbl::validation_type::exception_type);
 }
 
 
@@ -299,9 +197,9 @@ typedef safe<
 	min_validation<short, integral_c<short, 3> >,
 	no_initial<short>
 > safe_short;
-void test_const(std::ostream& out)
+
+TEST(SafeDataTest, Const)
 {
-	out << " - invalid initial value test - " << endl;
 // invalid initial value: 0 is less than 3
 // debug will throw exception
 /*
@@ -328,14 +226,10 @@ void test_const(std::ostream& out)
 */
 	safe_short i(3);
 
-	BOOST_ASSERT(i == 3);
-	out << "constructed value: " << i << endl;
-
-	out << endl;
+	EXPECT_EQ(3, i); // constructed value
 }
 
-#pragma warning (disable:4996)
-#include <boost/date_time/gregorian/gregorian.h>
+#include <boost/date_time/gregorian/gregorian.hpp>
 
 using boost::gregorian::date;
 
@@ -351,32 +245,17 @@ SAFE_DATA_INITIAL_VALUE(date_initial, date, boost::gregorian::day_clock::local_d
 typedef safe<date, date_validation> uninitialized_date;
 typedef safe<date, date_validation, date_initial> safe_date;
 
-void test_date (std::ostream& out)
+TEST(SafeDataTest, Date)
 {
-	out << " - date test - " << endl;
 // debug will throw exception
 
 #ifndef NDEBUG
- try {
-     uninitialized_date ud;
- }
- catch ( std::runtime_error const& e ) {
-     out << "expected exception caught: " << e.what() << endl;
- }
+    EXPECT_THROW(uninitialized_date ud, std::runtime_error);
 #else
- uninitialized_date ud;
- try {
-     ud.validate();
-     throw std::runtime_error("error: expected exception not thrown");
- }
- catch ( std::runtime_error const& e ) {
-     out << "expected exception caught: " << e.what() << endl;
- }
+   EXPECT_NO_THROW(uninitialized_date ud);
+   EXPECT_THROW(ud.validate(), std::runtime_error)
 #endif
 
 	safe_date sd;
-	BOOST_ASSERT(sd == date_initial());
-	out << "initial value: " << sd << endl;
-
-	out << endl;
+	EXPECT_EQ(date_initial(), sd);
 }
